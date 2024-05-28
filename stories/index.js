c47e3d5e4bccae186617b7ffa6a8f8102ab317ae
Leandro Filippi <leandrofilippi@LF.local>
2024-05-28 10:24:56 -0300

    v0.1.5

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
 export { Select } from "./Select/Select.tsx";
 export { default as PropertySearch } from "./PropertySearch/PropertySearch.tsx";
-export { default as ButtonHogar } from "./ButtonHogar/ButtonHogar";
+export { ButtonHogar } from "./ButtonHogar/ButtonHogar";
 export { InputHogar } from "./InputHogar/InputHogar.tsx";
