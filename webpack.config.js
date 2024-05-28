b1da50e3936b32ed8f142c5dcf9310c50ab2e4c6
Leandro Filippi <leandrofilippi@LF.local>
2024-05-28 09:23:52 -0300

    v0.1.2

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["svg-url-loader"],
+      },
     ],
   },
   resolve: {
